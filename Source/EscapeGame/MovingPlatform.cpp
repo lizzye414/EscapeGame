3b9a917b5cc181b9621c0960cedb7a3a0a51b794
Liz Elliott <lizzye414@icloud.com>
2020-05-29 15:29:27 +0100

    Added moving platforms

---
+++ Source/EscapeGame/MovingPlatform.cpp
@@ -0,0 +1,115 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "UObject/ConstructorHelpers.h"
+#include "Kismet/GameplayStatics.h"
+#include "GameFramework/Actor.h"
+#include "Components/SceneComponent.h"
+#include <string>
+#include "MovingPlatform.h"
+
+// Sets default values
+AMovingPlatform::AMovingPlatform()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	if (!RootComponent)
+	{
+		RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("MovingPlatform"));
+	}
+
+	Platform = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Platform"));
+	Platform->SetupAttachment(RootComponent);
+
+	Platform->SetRelativeLocation(FVector(0.0f, 0.0f, 0.0f));
+
+	AddHeight = 0.0f;
+	movingUp = true;
+
+}
+
+// Called when the game starts or when spawned
+void AMovingPlatform::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMovingPlatform::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (movingUp)
+	{
+		MoveUp(DeltaTime);
+	}
+
+	if (movingDown)
+	{
+		MoveDown(DeltaTime);
+	}
+
+}
+
+void AMovingPlatform::MoveUp(float dt)
+{
+	CurrentLocation = Platform->GetRelativeLocation();
+
+	AddHeight = dt * Speed;
+
+	if (FMath::IsNearlyEqual(CurrentLocation.Z, MaxHeight, 1.5f))
+	{
+		movingUp = false;
+		movingDown = true;
+	}
+	else if (movingUp)
+	{
+		
+		FVector NewLocation = FVector(0.0f, 0.0f, AddHeight);
+		Platform->AddRelativeLocation(NewLocation, false, 0, ETeleportType::None);
+	
+	}
+
+}
+
+void AMovingPlatform::MoveDown(float dt)
+{
+
+	CurrentLocation = Platform->GetRelativeLocation();
+
+	AddHeight = -dt * Speed;
+
+	if (!startUp)
+	{
+		if (FMath::IsNearlyEqual(CurrentLocation.Z, -MaxHeight + 100.0f, 1.5f))
+		{
+			movingUp = true;
+			movingDown = false;
+		}
+		else if (movingDown)
+		{
+
+			FVector NewLocation = FVector(0.0f, 0.0f, AddHeight);
+			Platform->AddRelativeLocation(NewLocation, false, 0, ETeleportType::None);
+
+		}
+	}
+	else
+	{
+		if (FMath::IsNearlyEqual(CurrentLocation.Z, 20.0f, 1.5f))
+		{
+			movingUp = true;
+			movingDown = false;
+		}
+		else if (movingDown)
+		{
+
+			FVector NewLocation = FVector(0.0f, 0.0f, AddHeight);
+			Platform->AddRelativeLocation(NewLocation, false, 0, ETeleportType::None);
+
+		}
+	}
+
+}
+
---
+++ Source/EscapeGame/MovingPlatform.h
@@ -0,0 +1,51 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Components/StaticMeshComponent.h"
+#include "MovingPlatform.generated.h"
+
+UCLASS()
+class ESCAPEGAME_API AMovingPlatform : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AMovingPlatform();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(EditAnywhere)
+	UStaticMeshComponent* Platform;
+
+	UFUNCTION()
+		void MoveUp(float dt);
+
+	UFUNCTION()
+		void MoveDown(float dt);
+
+	FVector CurrentLocation;
+	float AddHeight;
+
+	bool movingUp;
+	bool movingDown;
+	
+	UPROPERTY(EditAnywhere, Category = "Settings")
+	float Speed;
+
+	UPROPERTY(EditAnywhere, Category = "Settings")
+		float MaxHeight;
+
+	UPROPERTY(EditAnywhere, Category = "Settings")
+		bool startUp;
+
+};
